b645e77aee02d01a50f1a1c159972928429bb34f
Aigelov <aigelov@gmail.com>
2019-11-30 15:38:57 +0600

    Fetch, xhr

---
+++ 14-fetch.js
@@ -0,0 +1,33 @@
+const requestUrl = 'https://jsonplaceholder.typicode.com/users';
+function sendRequest(method, url, body = null) {
+  const headers = {
+    'Content-Type': 'application/json'
+  };
+  return fetch(url, {
+      method: method,
+      body: JSON.stringify(body),
+      headers: headers
+    })
+    .then(response => {
+      if (response.ok) {
+        return response.json()
+      }
+      return response.json()
+        .then(error => {
+          const err = new Error('Something went wrong');
+          err.data = error;
+          throw err;
+        })
+    });
+}
+// sendRequest('GET', requestUrl)
+//   .then(data => console.log(data))
+//   .catch(err => console.error(err));
+
+const body = {
+  name: 'Vladilen',
+  age: 26
+};
+sendRequest('POST', requestUrl, body)
+  .then(data => console.log(data))
+  .catch(err => console.error(err));
